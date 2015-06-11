983e13228f54877e8d1b2f4d2bbea78ee1de6961
acewf <ace.wf.home@gmail.com>
2015-06-11 23:59:49 +0100

    prepare to language dev

---
+++ app/includes/top-menu.php
@@ -0,0 +1,17 @@
+<?php
+ /* FOOTER */ ?>
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+<nav class="choose-menu">
+    <a href="https://www.secure-hotel-booking.com/smart/Casa-do-Principe/2E3B/en/" target="_blank">
+        <h3>Book a Room</h3>
+    </a>
+    <ul>
+        <li><a href="<?php echo "$basePath"; ?>home">Home</a></li>
+        <li><a href="<?php echo "$basePath"; ?>casa-do-principe">Casa do príncipe</a></li>
+        <li><a href="<?php echo "$basePath"; ?>suites-rooms">Suites and rooms</a></li>
+        <li><a href="<?php echo "$basePath"; ?>gallery">Gallery</a></li>
+        <li><a href="<?php echo "$basePath"; ?>location">Location</a></li>
+        <li><a href="<?php echo "$basePath"; ?>personalize-your-stay">Personalize your stay</a></li>
+        <li><a href="<?php echo "$basePath"; ?>contacts">Contacts</a></li>
+    </ul>
+</nav>
--- app/index.php
+++ app/index.php
@@ -54,20 +54,7 @@
             </div>
             <div id="menu-options" class="overlay yellow">
                 <button class="close"></button>
-                <nav class="choose-menu">
-                    <a href="https://www.secure-hotel-booking.com/smart/Casa-do-Principe/2E3B/en/" target="_blank">
-                        <h3>Book a Room</h3>
-                    </a>
-                    <ul>
-                        <li><a href="<?php echo "$basePath"; ?>home">Home</a></li>
-                        <li><a href="<?php echo "$basePath"; ?>casa-do-principe">Casa do príncipe</a></li>
-                        <li><a href="<?php echo "$basePath"; ?>suites-rooms">Suites and rooms</a></li>
-                        <li><a href="<?php echo "$basePath"; ?>gallery">Gallery</a></li>
-                        <li><a href="<?php echo "$basePath"; ?>location">Location</a></li>
-                        <li><a href="<?php echo "$basePath"; ?>personalize-your-stay">Personalize your stay</a></li>
-                        <li><a href="<?php echo "$basePath"; ?>contacts">Contacts</a></li>
-                    </ul>
-                </nav>
+                <?php include(dirname(__FILE__).'/includes/top-menu.php'); ?>
             </div>
             <div class="main">
                 <header>
